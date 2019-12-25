9b3fe9b7ccd28cff598c2e537f01dfec8812bbf9
blockmood <yuexingphp@gmail.com>
2019-12-25 10:19:34 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -11,35 +11,35 @@
    
    import lazyImg from '../../components/lazy-img'
 
-export default {
-    data (){
-        return {
-            group: [
-                {
-                    src: "//img-cdn-qiniu.dcloud.net.cn/static/images/side/uniapp-mall.png",
-                },
-                {
-                    src:"https://oimageb6.ydstatic.com/image?id=-618308228018432514&product=adpublish&w=520&h=347"
-                },
-                {
-                    src:'https://camo.githubusercontent.com/c156285977609c7bb08a2f9ff8970445d8fd6be7/687474703a2f2f696d672d63646e2d71696e69752e64636c6f75642e6e65742e636e2f756e696170702f74656d706c6174652f6e65772e706e67'
-                },
-                {
-                    src:"https://cn.vuejs.org/images/logo.png"
-                },
-                {
-                    src: "//img-cdn-qiniu.dcloud.net.cn/static/images/side/uniapp-mall.png",
-                },
-                {
-                    src:"https://oimageb6.ydstatic.com/image?id=-618308228018432514&product=adpublish&w=520&h=347"
-                },
-                {
-                    src:'https://camo.githubusercontent.com/c156285977609c7bb08a2f9ff8970445d8fd6be7/687474703a2f2f696d672d63646e2d71696e69752e64636c6f75642e6e65742e636e2f756e696170702f74656d706c6174652f6e65772e706e67'
-                },
-                {
-                    src:"https://cn.vuejs.org/images/logo.png"
-                }
-            ]
+    export default {
+        data (){
+            return {
+                group: [
+                    {
+                        src: "//img-cdn-qiniu.dcloud.net.cn/static/images/side/uniapp-mall.png",
+                    },
+                    {
+                        src:"https://oimageb6.ydstatic.com/image?id=-618308228018432514&product=adpublish&w=520&h=347"
+                    },
+                    {
+                        src:'https://camo.githubusercontent.com/c156285977609c7bb08a2f9ff8970445d8fd6be7/687474703a2f2f696d672d63646e2d71696e69752e64636c6f75642e6e65742e636e2f756e696170702f74656d706c6174652f6e65772e706e67'
+                    },
+                    {
+                        src:"https://cn.vuejs.org/images/logo.png"
+                    },
+                    {
+                        src: "//img-cdn-qiniu.dcloud.net.cn/static/images/side/uniapp-mall.png",
+                    },
+                    {
+                        src:"https://oimageb6.ydstatic.com/image?id=-618308228018432514&product=adpublish&w=520&h=347"
+                    },
+                    {
+                        src:'https://camo.githubusercontent.com/c156285977609c7bb08a2f9ff8970445d8fd6be7/687474703a2f2f696d672d63646e2d71696e69752e64636c6f75642e6e65742e636e2f756e696170702f74656d706c6174652f6e65772e706e67'
+                    },
+                    {
+                        src:"https://cn.vuejs.org/images/logo.png"
+                    }
+                ]
         }
     },
     methods:{
